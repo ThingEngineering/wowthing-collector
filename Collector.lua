7a1a7a61b9bba3662f5ce44fb8ca0835c6ad39d4
Freddie <freddie@wafflemonster.org>
2018-04-24 16:11:35 +1000

    Fix Reagent Bank contents not updating correctly

--- Collector.lua
+++ Collector.lua
@@ -843,7 +843,7 @@ function wwtc:ScanBag(bagID)
     if charData == nil then return end
 
     -- Short circuit if bank isn't open
-    if (bagID == -3 or bagID == -1 or (bagID >= 5 and bagID <= 11)) and not bankOpen then
+    if (bagID == -1 or (bagID >= 5 and bagID <= 11)) and not bankOpen then
         return
     end
 
