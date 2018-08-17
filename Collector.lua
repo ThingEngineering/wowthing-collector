87151f045d6b25e8aa13791239bb04f83ecbed46
Freddie <freddie@wafflemonster.org>
2018-08-17 08:48:23 -0700

    Add BfA currency scanning

--- Collector.lua
+++ Collector.lua
@@ -71,6 +71,12 @@ local currencies = {
     1508, -- Veiled Argunite
     1533, -- Wakening Essence
     1501, -- Writhing Essence
+    -- Battle for Azeroth
+    1565, -- Rich Azerite Fragment
+    1710, -- Seafarer's Dubloon
+    1580, -- Seal of Wartorn Fate
+    1560, -- War Resources
+    1587, -- War Supplies
 }
 
 -- Trade skill cooldowns
