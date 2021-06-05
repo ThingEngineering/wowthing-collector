a6eb3ea0bf75e508d755c0fbd1c9fc76e5dae6de
Freddie <freddie@wafflemonster.org>
2021-06-05 14:39:01 -0700

    Bump version
    - Fix TOC

--- Collector.lua
+++ Collector.lua
@@ -12,7 +12,7 @@ local LibRealmInfo = LibStub('LibRealmInfo17janekjl')
 
 -- Default SavedVariables
 local defaultWWTCSaved = {
-    version = 9051,
+    version = 9052,
     chars = {},
     guilds = {},
     heirlooms = {},
