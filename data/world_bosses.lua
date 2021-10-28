bbb82f3fa794a2e52961800d16989f55f9bb25a1
Freddie <freddie@wafflemonster.org>
2021-10-27 22:12:59 -0700

    Update world boss list

--- data/world_bosses.lua
+++ data/world_bosses.lua
@@ -3,40 +3,73 @@ local an, ns = ...
 
 -- World boss quests
 ns.worldBossQuests = {
+    -- Mists of Pandaria
+    [32099] = { 104001, "Sha of Anger", "Sha of Anger" },
+    [32098] = { 104002, "Galleon", "Galleon" },
+    [32518] = { 104003, "Nalak", "Nalak" },
+    [32519] = { 104004, "Oondasta", "Oondasta" },
+
     -- Warlords of Draenor
-    --[37460] = "Gorgrond Bosses", -- Drov the Ruinator
-    --[37462] = "Gorgrond Bosses", -- Tarlna
-    --[37464] = "Rukhmar",
-    --[94015] = "Supreme Lord Kazzak",
+    [37460] = { 105001, "Gorgrond World Bosses", "Drov the Ruinator" },
+    [37462] = { 105001, "Gorgrond World Bosses", "Tarlna the Ageless" },
+    [37464] = { 105002, "Rukhmar", "Rukhmar" },
+    [39380] = { 105003, "Supreme Lord Kazzak", "Supreme Lord Kazzak" },
 
     -- Legion
-    --[42269] = "Legion Bosses", -- The Soultakers
-    --[42270] = "Legion Bosses", -- Nithogg
-    --[42779] = "Legion Bosses", -- Shar'thos
-    --[42819] = "Legion Bosses", -- Humongris
-    --[43192] = "Legion Bosses", -- Levantus
-    --[43193] = "Legion Bosses", -- Calamir
-    --[43448] = "Legion Bosses", -- Drugon the Frostblood
-    --[43512] = "Legion Bosses", -- Ana-Mouz
-    --[43513] = "Legion Bosses", -- Na'zak the Fiend
-    --[43985] = "Legion Bosses", -- Flotsam
-    --[44287] = "Legion Bosses", -- Withered Jim
+    [42269] = { 106001, "Legion World Bosses", "The Soultakers" },
+    [42270] = { 106001, "Legion World Bosses", "Nithogg" },
+    [42779] = { 106001, "Legion World Bosses", "Shar'thos" },
+    [42819] = { 106001, "Legion World Bosses", "Humongris" },
+    [43192] = { 106001, "Legion World Bosses", "Levantus" },
+    [43193] = { 106001, "Legion World Bosses", "Calamir" },
+    [43448] = { 106001, "Legion World Bosses", "Drugon the Frostblood" },
+    [43512] = { 106001, "Legion World Bosses", "Ana-Mouz" },
+    [43513] = { 106001, "Legion World Bosses", "Na'zak the Fiend" },
+    [43985] = { 106001, "Legion World Bosses", "Flotsam" },
+    [44287] = { 106001, "Legion World Bosses", "Withered Jim" },
 
-    --[49166] = "Greater Invasions", -- Inquisitor Meto
-    --[49167] = "Greater Invasions", -- Mistress Alluradel
-    --[49168] = "Greater Invasions", -- Pit Lord Vilemus
-    --[49169] = "Greater Invasions", -- Matron Folnuna
-    --[49170] = "Greater Invasions", -- Occularus
-    --[49171] = "Greater Invasions", -- Sotanathor
+    [46945] = { 106002, "Broken Shore World Bosses", "Si'vash" },
+    [46947] = { 106002, "Broken Shore World Bosses", "Brutallus" },
+    [46948] = { 106002, "Broken Shore World Bosses", "Malificus" },
+    [47061] = { 106002, "Broken Shore World Bosses", "Apocron" },
 
-    --[48799] = "Pristine Argunite", -- Fuel of a Doomed World weekly
+    [49166] = { 106003, "Argus Greater Invasions", "Inquisitor Meto" },
+    [49167] = { 106003, "Argus Greater Invasions", "Mistress Alluradel" },
+    [49168] = { 106003, "Argus Greater Invasions", "Pit Lord Vilemus" },
+    [49169] = { 106003, "Argus Greater Invasions", "Matron Folnuna" },
+    [49170] = { 106003, "Argus Greater Invasions", "Occularus" },
+    [49171] = { 106003, "Argus Greater Invasions", "Sotanathor" },
 
     -- Battle for Azeroth
+    [52181] = { 107001, "BfA World Bosses", "T'Zane" },
+    [52169] = { 107001, "BfA World Bosses", "Ji'arak" },
+    [52157] = { 107001, "BfA World Bosses", "Hailstone Construct" },
+    [52163] = { 107001, "BfA World Bosses", "Azurethos" },
+    [52166] = { 107001, "BfA World Bosses", "Warbringer Yenajz" },
+    [52196] = { 107001, "BfA World Bosses", "Dunegorger Kraulok" },
+
+    [52848] = { 107002, "Arathi World Bosses", "The Lion's Roar" },
+    [52847] = { 107002, "Arathi World Bosses", "Doom's Howl" },
+
+    [54896] = { 107003, "Darkshore World Bosses", "Ivus the Forest Lord" },
+    [54895] = { 107003, "Darkshore World Bosses", "Ivus the Decayed" },
+
+    [56057] = { 107004, "Nazjatar World Bosses", "Ulmath, the Soulbinder" },
+    [56056] = { 107004, "Nazjatar World Bosses", "Wekemara" },
+
+    [55466] = { 107005, "Uldum World Bosses", "Vuk'laz the Earthbreaker" },
+
+    [58705] = { 107005, "Vale World Bosses", "Grand Empress Shek'zara" },
 
     -- Shadowlands
     [61813] = { 108001, "Shadowlands World Bosses", "Valinor, the Light of Eons" },
     [61814] = { 108001, "Shadowlands World Bosses", "Nurgash Muckformed" },
     [61815] = { 108001, "Shadowlands World Bosses", "Oranomonos the Everbranching" },
     [61816] = { 108001, "Shadowlands World Bosses", "Mortanis" },
-    [64531] = { 108002, "Mor'geth", "Mor'geth, Tormentor of the Damned" },
+
+    [63414] = { 108002, "Wrath of the Jailer", "Wrath of the Jailer" },
+
+    [63854] = { 108003, "Tormentors of Torghast", "Tormentors of Torghast" },
+
+    [64531] = { 108004, "Mor'geth", "Mor'geth, Tormentor of the Damned" },
 }
