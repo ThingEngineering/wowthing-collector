20c04c5f70dae1aaac768ecc68b1366688afd999
Freddie <freddie@wafflemonster.org>
2022-02-13 11:40:37 -0800

    Add Headless Horseman, Frost Lord Ahune, and Coren DIrebrew to LFG scanning

--- data/instances.lua
+++ data/instances.lua
@@ -3,6 +3,15 @@ local an, ns = ...
 
 ns.instances = {
     {
+        dungeonId = 285,
+    }, -- The Headless Horseman
+    {
+        dungeonId = 286,
+    }, -- The Frost Lord Ahune
+    {
+        dungeonId = 287,
+    }, -- Coren Direbrew
+    {
         dungeonId = 288,
-    },
+    }, -- Crown Chemical Co.
 }
