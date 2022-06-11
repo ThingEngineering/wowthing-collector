52f3f54cb8a86b768f482f9aa7423e1ac645cf56
Freddie <freddie@wafflemonster.org>
2022-06-11 16:18:50 -0700

    Move reputations to data file

--- Collector.lua
+++ Collector.lua
@@ -75,50 +75,6 @@ local checkPets = {
     [216] = 33239, -- Argent Gruntling
     [1350] = 73809, -- Sky Lantern
 }
-local checkReputations = {
-    --1492, -- Emperor Shaohao
-}
-local checkFriendships = {
-    2463, -- Marasmius
-}
-local paragonReputations = {
-    -- Legion
-    1828, -- Highmountain Tribe
-    1859, -- The Nightfallen
-    1883, -- Dreamweavers
-    1894, -- The Wardens
-    1900, -- Court of Farondis
-    1948, -- Valarjar
-    2045, -- Armies of Legionfall
-    2165, -- Army of the Light
-    2170, -- Argussian Reach
-
-    -- Battle for Azeroth
-    2103, -- Zandalari Empire
-    2156, -- Talanji's Expedition
-    2157, -- The Honorbound
-    2158, -- Voldunai
-    2159, -- 7th Legion
-    2160, -- Proudmoore Admiralty
-    2161, -- Order of Embers
-    2162, -- Storm's Wake
-    2163, -- Tortollan Seekers
-    2164, -- Champions of Azeroth
-    2373, -- The Unshackled
-    2391, -- Rustbolt Resistance
-    2400, -- Waveblade Ankoan
-    2415, -- Rajani
-    2417, -- Uldum Accord
-
-    -- Shadowlands
-    2407, -- The Ascended
-    2410, -- The Undying Army
-    2413, -- Court of Harvesters
-    2432, -- Ve'nari
-    2465, -- The Wild Hunt
-    2470, -- Death's Advance
-    2472, -- The Archivists' Codex
-}
 local worldQuestFactions = {
     -- Both
     [50562] = 2164, -- Champions of Azeroth
@@ -1492,17 +1448,17 @@ function wwtc:ScanReputations()
     charData.paragons = {}
     charData.reputations = {}
 
-    for _, factionID in ipairs(checkReputations) do
+    for _, factionID in ipairs(ns.reputations) do
         local _, _, _, _, _, barValue = GetFactionInfoByID(factionID)
         charData.reputations[factionID] = barValue
     end
 
-    for _, friendshipID in ipairs(checkFriendships) do
+    for _, friendshipID in ipairs(ns.friendships) do
         local _, friendRep = GetFriendshipReputation(friendshipID)
         charData.reputations[friendshipID] = friendRep
     end
 
-    for i, factionID in ipairs(paragonReputations) do
+    for i, factionID in ipairs(ns.paragonReputations) do
         if C_Reputation.IsFactionParagon(factionID) then 
             local currentValue, threshold, _, hasRewardPending = C_Reputation.GetFactionParagonInfo(factionID)
             -- value:max:hasReward
