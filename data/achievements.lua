dbdd98a6bcf4ed4a0928cbfdf3d789aa0886522c
Freddie <freddie@wafflemonster.org>
2022-09-06 17:18:33 -0700

    Add more Legion artifact hidden achievement scanning

--- data/achievements.lua
+++ data/achievements.lua
@@ -7,11 +7,15 @@ ns.achievements = {
 
     -- Legion
     10596, -- Bigger Fish to Fry
+    10747, -- Fighting with Style: Upgraded
     11152, -- Artifact - Hidden - Dungeons
     11153, -- Artifact - Hidden - World Quests
     11154, -- Artifact - Hidden - Kills?
     11160, -- Artifact - Balance of Power - World Bosses
     11162, -- Artifact - Balance of Power - Mythic+ 15 timed
+    11657, -- Artifact - Challenge - Color 2
+    11661, -- Artifact - Challenge - Color 3
+    11665, -- Artifact - Challenge - Color 4
 
     -- Shadowlands
     14765, -- Ramparts Racer
