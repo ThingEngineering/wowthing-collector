625c548d375bec25a63e68d1344a39d4d957354e
Freddie <freddie@wafflemonster.org>
2022-09-20 18:17:43 -0700

    WIP MoP cooking sub-professions

---
+++ data/professions.lua
@@ -0,0 +1,11 @@
+local an, ns = ...
+
+
+ns.professions = {
+	975, -- Way of the Grill
+	976, -- Way of the Wok
+	977, -- Way of the Pot
+	978, -- Way of the Steamer
+	979, -- Way of the Oven
+	980, -- Way of the Brew
+}
