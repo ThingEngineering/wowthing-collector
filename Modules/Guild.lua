d84b76d8281f16d29a01bfcc6eeed1d07c00756e
Freddie <freddie@wafflemonster.org>
2023-05-21 22:56:33 -0700

    Add guild bank scanning
    - Update guild data on entering world

--- Modules/Guild.lua
+++ Modules/Guild.lua
@@ -2,8 +2,43 @@ local Addon = LibStub('AceAddon-3.0'):GetAddon('WoWthing_Collector')
 local Module = Addon:NewModule('Guild')
 
 
+local C_TooltipInfo_GetGuildBankItem = C_TooltipInfo.GetGuildBankItem
+-- There's probably a Blizzard constant for this somewhere
+local SLOTS_PER_GUILD_BANK_TAB = 98
+
 function Module:OnEnable()
+    self.hasQueried = false
+    self.isBankOpen = false
+
+    self:RegisterEvent('PLAYER_INTERACTION_MANAGER_FRAME_HIDE')
+    self:RegisterEvent('PLAYER_INTERACTION_MANAGER_FRAME_SHOW')
     self:RegisterEvent('PLAYER_GUILD_UPDATE')
+
+    self:RegisterBucketEvent({ 'GUILDBANKBAGSLOTS_CHANGED' }, 1, 'UpdateGuildBank')
+end
+
+function Module:OnEnteringWorld()
+    self:UpdateGuild()
+end
+
+function Module:PLAYER_INTERACTION_MANAGER_FRAME_HIDE(_, interactionType)
+    if interactionType ~= Enum.PlayerInteractionType.GuildBanker then return end
+
+    self.isBankOpen = false
+end
+
+function Module:PLAYER_INTERACTION_MANAGER_FRAME_SHOW(_, interactionType)
+    if interactionType ~= Enum.PlayerInteractionType.GuildBanker then return end
+
+    self.hasQueried = false
+    self.isBankOpen = true
+
+    self.guild.copper = GetGuildBankMoney()
+
+    for i = 1, GetNumGuildBankTabs() do
+        local name, icon, isViewable, canDeposit, numWithdrawals, remainingWithdrawals = GetGuildBankTabInfo(i)
+        self.guild.tabs["tab "..i] = { name, icon }
+    end
 end
 
 function Module:PLAYER_GUILD_UPDATE(_, unitTarget)
@@ -28,7 +63,58 @@ function Module:UpdateGuild()
         WWTCSaved.guilds[guildName].items = WWTCSaved.guilds[guildName].items or {}
         WWTCSaved.guilds[guildName].scanTimes = WWTCSaved.guilds[guildName].scanTimes or {}
         WWTCSaved.guilds[guildName].tabs = WWTCSaved.guilds[guildName].tabs or {}
+        self.guild = WWTCSaved.guilds[guildName]
+    else
+        self.guild = nil
     end
 
     Addon.charData.guildName = guildName
+end
+
+-- Scan guild bank tabs
+function Module:UpdateGuildBank()
+    print('UpdateGuildBank')
+
+    -- Short circuit if guild bank isn't open
+    -- if not guildBankOpen then
+    --     return
+    -- end
+
+    local now = time()
+    self.guild.scanTimes['bank'] = now
+
+    -- Request data for every tab, but only once per guild bank opening or we scan infinitely
+    if self.hasQueried == false then
+        for tabIndex = 1, GetNumGuildBankTabs() do
+            QueryGuildBankTab(tabIndex)
+        end
+        self.hasQueried = true
+    end
+
+    for tabIndex = 1, GetNumGuildBankTabs() do
+        local tabKey = "t"..tabIndex
+        self.guild.items[tabKey] = {}
+        local tab = self.guild.items[tabKey]
+
+        for slotIndex = 1, SLOTS_PER_GUILD_BANK_TAB do
+            local link = GetGuildBankItemLink(tabIndex, slotIndex)
+            if link ~= nil then
+                if string.find(link, '\Hitem:82800:') then
+                    local tooltipData = C_TooltipInfo_GetGuildBankItem(tabIndex, slotIndex)
+
+                    tab["s"..slotIndex] = table.concat({
+                        'pet',
+                        tooltipData['battlePetSpeciesID'],
+                        tooltipData['battlePetLevel'],
+                        tooltipData['battlePetBreedQuality'],
+                    }, ':')
+
+                else
+                    local _, itemCount, _, _, itemQuality = GetGuildBankItemInfo(tabIndex, slotIndex)
+                    local parsed = Addon:ParseItemLink(link, itemQuality or -1, itemCount or 1)
+                    tab["s"..slotIndex] = parsed
+                end
+            end
+        end
+    end
 end
