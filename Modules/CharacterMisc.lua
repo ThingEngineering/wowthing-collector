c310feb108fbdff3171f0d7de973f7a4f9b0269e
Freddie <freddie@wafflemonster.org>
2023-05-21 19:22:08 -0700

    Add CharacterMisc module

---
+++ Modules/CharacterMisc.lua
@@ -0,0 +1,35 @@
+local Addon = LibStub('AceAddon-3.0'):GetAddon('WoWthing_Collector')
+local Module = Addon:NewModule('CharacterFlags')
+
+
+function Module:OnEnable()
+    self:RegisterEvent('PLAYER_FLAGS_CHANGED')
+    self:RegisterEvent('PLAYER_INTERACTION_MANAGER_FRAME_HIDE')
+
+    self:RegisterBucketEvent({ 'QUEST_LOG_UPDATE' }, 2, 'UpdateChromieTime')
+end
+
+function Module:OnEnteringWorld()
+    self:UpdateChromieTime()
+    self:UpdateWarMode()
+end
+
+function Module:PLAYER_FLAGS_CHANGED(unitId)
+    if unitId == 'player' then
+        self:UpdateWarMode()
+    end
+end
+
+function Module:PLAYER_INTERACTION_MANAGER_FRAME_HIDE(_, interactionType)
+    if type == Enum.PlayerInteractionType.ChromieTime then
+        self:UpdateChromieTime()
+    end
+end
+
+function Module:UpdateChromieTime()
+    Addon.charData.chromieTime = UnitChromieTimeID("player")
+end
+
+function Module:UpdateWarMode()
+    Addon.charData.isWarMode = C_PvP.IsWarModeDesired()
+end
