99d75980c196f8bfa8abfc952f17f6d41053a046
Freddie <freddie@wafflemonster.org>
2023-05-29 18:30:55 -0700

    Add ProfessionTraits module

---
+++ DB/ProfessionTraits.lua
@@ -0,0 +1,49 @@
+local Module = LibStub('AceAddon-3.0'):GetAddon('WoWthing_Collector'):GetModule('ProfessionTraits')
+
+
+Module.db.skillLines = {
+    -- Alchemy
+    [171] = {
+        2823, -- Dragonflight
+    },
+    -- Blacksmithing
+    [164] = {
+        2822, -- Dragonflight
+    },
+    -- Enchanting
+    [333] = {
+        2825, -- Dragonflight
+    },
+    -- Engineering
+    [202] = {
+        2827, -- Dragonflight
+    },
+    -- Herbalism
+    [182] = {
+        2832, -- Dragonflight
+    },
+    -- Inscription
+    [773] = {
+        2828, -- Dragonflight
+    },
+    -- Jewelcrafting
+    [755] = {
+        2829, -- Dragonflight
+    },
+    -- Leatherworking
+    [165] = {
+        2830, -- Dragonflight
+    },
+    -- Mining
+    [186] = {
+        2833, -- Dragonflight
+    },
+    -- Skinning
+    [393] = {
+        2834, -- Dragonflight
+    },
+    -- Tailoring
+    [197] = {
+        2831, -- Dragonflight
+    },
+}
---
+++ Modules/ProfessionTraits.lua
@@ -0,0 +1,70 @@
+local Addon = LibStub('AceAddon-3.0'):GetAddon('WoWthing_Collector')
+local Module = Addon:NewModule('ProfessionTraits')
+
+
+Module.db = {}
+
+function Module:OnEnable()
+    self:RegisterBucketEvent(
+        {
+            'SKILL_LINES_CHANGED',
+            'SKILL_LINE_SPEC_RANKS_CHANGED',
+            'TRAIT_CONFIG_UPDATED',
+        },
+        2,
+        'UpdateProfessions'
+    )
+end
+
+function Module:OnEnteringWorld()
+    self:UpdateProfessions()
+end
+
+function Module:UpdateProfessions()
+    Addon.charData.professionTraits = {}
+
+    local profession1, profession2 = GetProfessions()
+    self:UpdateTraits(profession1)
+    self:UpdateTraits(profession2)
+end
+
+function Module:UpdateTraits(spellbookIndex)
+    if spellbookIndex == nil then return end
+
+    local professionSkillLineId = select(7, GetProfessionInfo(spellbookIndex))
+    if not self.db.skillLines[professionSkillLineId] then
+        print("No skill lines? "..(professionSkillLineId or nil))
+    end
+
+    for _, skillLineId in ipairs(self.db.skillLines[professionSkillLineId]) do
+        local skillLineData = {}
+
+        local configId = C_ProfSpecs.GetConfigIDForSkillLine(skillLineId)
+        local specTabIds = C_ProfSpecs.GetSpecTabIDsForSkillLine(skillLineId)
+
+        for _, specTabId in ipairs(specTabIds) do
+            local pathQueue = {
+                C_ProfSpecs.GetRootPathForTab(specTabId),
+            }
+
+            while #pathQueue > 0 do
+                local pathId = tremove(pathQueue, 1)
+
+                local childPathIds = C_ProfSpecs.GetChildrenForPath(pathId)
+                for _, childPathId in ipairs(childPathIds) do
+                    table.insert(pathQueue, childPathId)
+                end
+
+                local nodeInfo = C_Traits.GetNodeInfo(configId, pathId)
+                if nodeInfo ~= nil then
+                    table.insert(skillLineData, table.concat({
+                        nodeInfo.ID,
+                        nodeInfo.ranksPurchased,
+                    }, ':'))
+                end
+            end
+        end
+        
+        Addon.charData.professionTraits[skillLineId] = table.concat(skillLineData, '|')
+    end
+end
