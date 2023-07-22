63ecdbdfc29a90138d5a92704e780d05195270d8
Freddie <freddie@wafflemonster.org>
2023-07-22 13:52:23 -0700

    Add Illusions module

---
+++ Modules/Illusions.lua
@@ -0,0 +1,33 @@
+local Addon = LibStub('AceAddon-3.0'):GetAddon('WoWthing_Collector')
+local Module = Addon:NewModule('Illusions')
+
+
+function Module:OnEnable()
+    self:RegisterBucketEvent(
+        {
+            'TRANSMOG_COLLECTION_SOURCE_ADDED',
+            'TRANSMOG_COLLECTION_SOURCE_REMOVED',
+            'TRANSMOG_COLLECTION_UPDATED',
+        },
+        2,
+        'UpdateIllusions'
+    )
+end
+
+function Module:OnEnteringWorld()
+    self:UpdateIllusions()
+end
+
+function Module:UpdateIllusions()
+    Addon.charData.scanTimes["illusions"] = time()
+
+    local illusions = {}
+    local illusionData = C_TransmogCollection.GetIllusions()
+    for _, illusion in ipairs(illusionData) do
+        if illusion.isCollected then
+            table.insert(illusions, illusion.sourceID)
+        end
+    end
+
+    Addon.charData.illusions = illusions
+end
