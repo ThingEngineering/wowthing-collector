472c67eda0d8340ce8e97972397edd054bdce360
Freddie <freddie@wafflemonster.org>
2023-11-04 16:03:27 -0700

    Use BatchWork for guild bank scanning

--- Modules/Guild.lua
+++ Modules/Guild.lua
@@ -14,7 +14,7 @@ function Module:OnEnable()
     self:RegisterEvent('PLAYER_INTERACTION_MANAGER_FRAME_SHOW')
     self:RegisterEvent('PLAYER_GUILD_UPDATE')
 
-    self:RegisterBucketEvent({ 'GUILDBANKBAGSLOTS_CHANGED' }, 1, 'UpdateGuildBank')
+    self:RegisterBucketEvent({ 'GUILDBANKBAGSLOTS_CHANGED' }, 2, 'UpdateGuildBank')
 end
 
 function Module:OnEnteringWorld()
@@ -34,11 +34,6 @@ function Module:PLAYER_INTERACTION_MANAGER_FRAME_SHOW(_, interactionType)
     self.isBankOpen = true
 
     self.guild.copper = GetGuildBankMoney()
-
-    for i = 1, GetNumGuildBankTabs() do
-        local name, icon, isViewable, canDeposit, numWithdrawals, remainingWithdrawals = GetGuildBankTabInfo(i)
-        self.guild.tabs["tab "..i] = { name, icon }
-    end
 end
 
 function Module:PLAYER_GUILD_UPDATE(_, unitTarget)
@@ -81,6 +76,8 @@ function Module:UpdateGuildBank()
     local now = time()
     self.guild.scanTimes['bank'] = now
 
+    local workload = {}
+
     -- Request data for every tab, but only once per guild bank opening or we scan infinitely
     if self.hasQueried == false then
         for tabIndex = 1, GetNumGuildBankTabs() do
@@ -90,29 +87,36 @@ function Module:UpdateGuildBank()
     end
 
     for tabIndex = 1, GetNumGuildBankTabs() do
-        local tabKey = "t"..tabIndex
-        self.guild.items[tabKey] = {}
-        local tab = self.guild.items[tabKey]
+        table.insert(workload, function()
+            local tabName, tabIcon, canView = GetGuildBankTabInfo(tabIndex)
+            self.guild.tabs['tab ' .. tabIndex] = { tabName, tabIcon }
+            if canView == false then return end
 
-        for slotIndex = 1, SLOTS_PER_GUILD_BANK_TAB do
-            local link = GetGuildBankItemLink(tabIndex, slotIndex)
-            if link ~= nil then
-                if string.find(link, '\Hitem:82800:') then
-                    local tooltipData = C_TooltipInfo_GetGuildBankItem(tabIndex, slotIndex)
+            local tabKey = 't' .. tabIndex
+            self.guild.items[tabKey] = {}
+            local tab = self.guild.items[tabKey]
 
-                    tab["s"..slotIndex] = table.concat({
-                        'pet',
-                        tooltipData['battlePetSpeciesID'],
-                        tooltipData['battlePetLevel'],
-                        tooltipData['battlePetBreedQuality'],
-                    }, ':')
+            for slotIndex = 1, SLOTS_PER_GUILD_BANK_TAB do
+                local link = GetGuildBankItemLink(tabIndex, slotIndex)
+                if link ~= nil then
+                    if string.find(link, '\Hitem:82800:') then
+                        local tooltipData = C_TooltipInfo_GetGuildBankItem(tabIndex, slotIndex)
 
-                else
-                    local _, itemCount, _, _, itemQuality = GetGuildBankItemInfo(tabIndex, slotIndex)
-                    local parsed = Addon:ParseItemLink(link, itemQuality or -1, itemCount or 1)
-                    tab["s"..slotIndex] = parsed
+                        tab["s" .. slotIndex] = table.concat({
+                            'pet',
+                            tooltipData['battlePetSpeciesID'],
+                            tooltipData['battlePetLevel'],
+                            tooltipData['battlePetBreedQuality'],
+                        }, ':')
+                    else
+                        local _, itemCount, _, _, itemQuality = GetGuildBankItemInfo(tabIndex, slotIndex)
+                        local parsed = Addon:ParseItemLink(link, itemQuality or -1, itemCount or 1)
+                        tab["s" .. slotIndex] = parsed
+                    end
                 end
             end
-        end
+        end)
     end
+    
+    Addon:BatchWork(workload)
 end
