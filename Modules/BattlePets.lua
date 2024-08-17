0ec7f57a8bc490412d788c379a8ee0b6e98d6244
Freddie <freddie@wafflemonster.org>
2024-08-16 18:13:19 -0700

    Add basic BattlePets module

---
+++ Modules/BattlePets.lua
@@ -0,0 +1,57 @@
+local Addon = LibStub('AceAddon-3.0'):GetAddon('WoWthing_Collector')
+local Module = Addon:NewModule('BattlePets')
+
+
+local CPJ_GetPetInfoByIndex = C_PetJournal.GetPetInfoByIndex
+local CPJ_GetPetInfoByPetID = C_PetJournal.GetPetInfoByPetID
+local CPJ_GetPetStats = C_PetJournal.GetPetStats
+
+function Module:OnEnable()
+    WWTCSaved.battlePets = WWTCSaved.battlePets or {}
+
+    self:RegisterEvent('NEW_PET_ADDED')
+    self:RegisterEvent('PET_JOURNAL_PET_DELETED')
+end
+
+function Module:OnEnteringWorld()
+    C_Timer.After(5, function() self:UpdateBattlePets() end)
+end
+
+function Module:NEW_PET_ADDED(_, guid)
+    local speciesId, _, level = CPJ_GetPetInfoByPetID(guid)
+    WWTCSaved.battlePets[Module:ParseGUID(guid)] = table.concat({
+        speciesId,
+        level,
+    }, ':')
+end
+
+function Module:PET_JOURNAL_PET_DELETED(_, guid)
+    WWTCSaved.battlePets[Module:ParseGUID(guid)] = nil
+end
+
+function Module:UpdateBattlePets()
+    if not C_PetJournal.IsJournalUnlocked() then return end
+
+    C_PetJournal.SetDefaultFilters()
+
+    local _, numOwned = C_PetJournal.GetNumPets()
+    local pets = {}
+    for petIndex = 1, numOwned do
+        local guid, speciesId, owned, _, level = CPJ_GetPetInfoByIndex(petIndex)
+        local _, _, _, _, quality = CPJ_GetPetStats(guid)
+        if guid ~= nil and owned then
+            pets[Module:ParseGUID(guid)] = table.concat({
+                speciesId,
+                level,
+                quality,
+            }, ':')
+        end
+    end
+
+    WWTCSaved.battlePets = pets
+end
+
+function Module:ParseGUID(guid)
+    local parts = { strsplit('-', guid) }
+    return tonumber(parts[3], 16)
+end
