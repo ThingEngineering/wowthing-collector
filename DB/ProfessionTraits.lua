fe03bc1dab3595a9f91622d8ecc53cf0cec89a20
Freddie <freddie@wafflemonster.org>
2024-08-29 14:14:22 -0700

    Scan Khaz Algar profession traits

--- DB/ProfessionTraits.lua
+++ DB/ProfessionTraits.lua
@@ -4,46 +4,57 @@ local Module = LibStub('AceAddon-3.0'):GetAddon('WoWthing_Collector'):GetModule(
 Module.db.skillLines = {
     -- Alchemy
     [171] = {
-        2823, -- Dragonflight
+        2823, -- Dragon Isles
+        2871, -- Khaz Algar
     },
     -- Blacksmithing
     [164] = {
-        2822, -- Dragonflight
+        2822, -- Dragon Isles
+        2872, -- Khaz Algar
     },
     -- Enchanting
     [333] = {
-        2825, -- Dragonflight
+        2825, -- Dragon Isles
+        2874, -- Khaz Algar
     },
     -- Engineering
     [202] = {
-        2827, -- Dragonflight
+        2827, -- Dragon Isles
+        2875, -- Khaz Algar
     },
     -- Herbalism
     [182] = {
-        2832, -- Dragonflight
+        2832, -- Dragon Isles
+        2877, -- Khaz Algar
     },
     -- Inscription
     [773] = {
-        2828, -- Dragonflight
+        2828, -- Dragon Isles
+        2878, -- Khaz Algar
     },
     -- Jewelcrafting
     [755] = {
-        2829, -- Dragonflight
+        2829, -- Dragon Isles
+        2879, -- Khaz Algar
     },
     -- Leatherworking
     [165] = {
-        2830, -- Dragonflight
+        2830, -- Dragon Isles
+        2880, -- Khaz Algar
     },
     -- Mining
     [186] = {
-        2833, -- Dragonflight
+        2833, -- Dragon Isles
+        2881, -- Khaz Algar
     },
     -- Skinning
     [393] = {
-        2834, -- Dragonflight
+        2834, -- Dragon Isles
+        2882, -- Khaz Algar
     },
     -- Tailoring
     [197] = {
-        2831, -- Dragonflight
+        2831, -- Dragon Isles
+        2883, -- Khaz Algar
     },
 }
