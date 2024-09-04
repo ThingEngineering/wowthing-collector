6ecaf3b56ccb79ad6c335700c1acde6df9c69162
Freddie <freddie@wafflemonster.org>
2024-09-04 13:59:09 -0700

    Add TWW contract auras

--- DB/Spells.lua
+++ DB/Spells.lua
@@ -27,6 +27,23 @@ Module.db.auras = {
     425308, -- Contract: Dream Wardens 1
     425306, -- Contract: Dream Wardens 2
     425305, -- Contract: Dream Wardens 3
+
+    -- The War Within
+    454928, -- Contract: Artisan's Consortium 1
+    454929, -- Contract: Artisan's Consortium 2
+    454930, -- Contract: Artisan's Consortium 3
+    454931, -- Contract: Council of Dornogal 1
+    454932, -- Contract: Council of Dornogal 2
+    454933, -- Contract: Council of Dornogal 3
+    454934, -- Contract: Assembly of the Deeps 1
+    454935, -- Contract: Assembly of the Deeps 2
+    454936, -- Contract: Assembly of the Deeps 3
+    454937, -- Contract: Hallowfall Arathi 1
+    454938, -- Contract: Hallowfall Arathi 2
+    454939, -- Contract: Hallowfall Arathi 3
+    454940, -- Contract: The Severed Threads 1
+    454941, -- Contract: The Severed Threads 2
+    454942, -- Contract: The Severed Threads 3
 }
 
 Module.db.gameTimeAuras = {
