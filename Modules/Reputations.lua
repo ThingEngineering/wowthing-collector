80bcdd662eb56270638891c27d2d958a491ea695
Freddie <freddie@wafflemonster.org>
2024-09-15 19:13:43 -0700

    Minor optimizations for Reputations module

--- Modules/Reputations.lua
+++ Modules/Reputations.lua
@@ -4,6 +4,11 @@ local Module = Addon:NewModule('Reputations')
 
 Module.db = {}
 
+local CGI_GetFriendshipReputation = C_GossipInfo.GetFriendshipReputation
+local CMF_GetMajorFactionRenownInfo = C_MajorFactions.GetMajorFactionRenownInfo
+local CR_GetFactionDataByID = C_Reputation.GetFactionDataByID
+local CR_GetFactionParagonInfo = C_Reputation.GetFactionParagonInfo
+
 function Module:OnEnable()
     self:RegisterBucketEvent('UPDATE_FACTION', 1, 'UpdateReputations')
 end
@@ -13,37 +18,40 @@ function Module:OnEnteringWorld()
 end
 
 function Module:UpdateReputations()
-    Addon.charData.scanTimes['reputations'] = time()
-
-    Addon.charData.paragons = {}
-    Addon.charData.reputations = {}
+    local reputations = {}
+    local paragons = {}
     
     for _, factionID in ipairs(self.db.reputations) do
-        local renownData = C_MajorFactions.GetMajorFactionRenownInfo(factionID)
+        local renownData = CMF_GetMajorFactionRenownInfo(factionID)
         if renownData ~= nil then
-            Addon.charData.reputations[factionID] = (renownData.renownLevel * renownData.renownLevelThreshold) + renownData.renownReputationEarned
+            reputations[factionID] = (renownData.renownLevel * renownData.renownLevelThreshold) + renownData.renownReputationEarned
         else
-            local factionData = C_Reputation.GetFactionDataByID(factionID)
+            local factionData = CR_GetFactionDataByID(factionID)
             if factionData ~= nil then
-                Addon.charData.reputations[factionID] = factionData.currentStanding
+                reputations[factionID] = factionData.currentStanding
             end
         end
     end
 
     for _, friendshipID in ipairs(self.db.friendships) do
-        local friendRep = C_GossipInfo.GetFriendshipReputation(friendshipID)
-        Addon.charData.reputations[friendshipID] = friendRep.standing
+        local friendRep = CGI_GetFriendshipReputation(friendshipID)
+        reputations[friendshipID] = friendRep.standing
     end
 
     for i, factionID in ipairs(self.db.paragon) do
         if C_Reputation.IsFactionParagon(factionID) then 
-            local currentValue, threshold, _, hasRewardPending = C_Reputation.GetFactionParagonInfo(factionID)
+            local currentValue, threshold, _, hasRewardPending = CR_GetFactionParagonInfo(factionID)
             -- value:max:hasReward
-            Addon.charData.paragons[factionID] = table.concat({
+            paragons[factionID] = table.concat({
                 currentValue or 0,
                 threshold or 0,
                 hasRewardPending and 1 or 0,
             }, ':')
         end
     end
+
+    Addon.charData.paragons = paragons
+    Addon.charData.reputations = reputations
+
+    Addon.charData.scanTimes.reputations = time()
 end
