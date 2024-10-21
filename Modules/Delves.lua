376a50dc59de31ad0fa53fd72070500ec53c8ec5
Freddie <freddie@wafflemonster.org>
2024-10-21 09:14:19 -0700

    Add Delve completion scanning

---
+++ Modules/Delves.lua
@@ -0,0 +1,27 @@
+local Addon = LibStub('AceAddon-3.0'):GetAddon('WoWthing_Collector')
+local Module = Addon:NewModule('Delves')
+
+
+function Module:OnEnable()
+    Addon.charData.delves = Addon.charData.delves or { [0] = { '0' } }
+
+    self:RegisterBucketEvent({ 'SCENARIO_COMPLETED' }, 2, 'UpdateScenario')
+end
+
+function Module:UpdateScenario()
+    local scenarioInfo = C_ScenarioInfo.GetScenarioInfo()
+    local widgetInfo = C_UIWidgetManager.GetScenarioHeaderDelvesWidgetVisualizationInfo(6183)
+    print('hello')
+    if scenarioInfo == nil or widgetInfo == nil then return end
+
+    local weeklyReset = time() + C_DateAndTime.GetSecondsUntilWeeklyReset()
+    Addon.charData.delves[weeklyReset] = Addon.charData.delves[weeklyReset] or {}
+
+    print('reset is '..weeklyReset)
+
+    tinsert(Addon.charData.delves[weeklyReset], table.concat({
+        scenarioInfo.scenarioID,
+        GetZoneText(),
+        widgetInfo.tierText,
+    }, '|'))
+end
