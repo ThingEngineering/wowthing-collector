513cecfcb86530761b6704cad7e4489c2b270b7e
Freddie <freddie@wafflemonster.org>
2025-03-29 09:25:16 -0700

    Add weekly delve gilded chest scanning

--- Modules/Delves.lua
+++ Modules/Delves.lua
@@ -2,10 +2,33 @@ local Addon = LibStub('AceAddon-3.0'):GetAddon('WoWthing_Collector')
 local Module = Addon:NewModule('Delves')
 
 
+local CUIWM_GetSpellDisplayVisualizationInfo = C_UIWidgetManager.GetSpellDisplayVisualizationInfo
+
 function Module:OnEnable()
     Addon.charData.delves = Addon.charData.delves or { [0] = { '0' } }
+    Addon.charData.delvesGilded = Addon.charData.delvesGilded or 0
 
+    self:RegisterEvent('CURRENCY_DISPLAY_UPDATE')
     self:RegisterBucketEvent({ 'SCENARIO_COMPLETED' }, 2, 'UpdateScenario')
+end
+
+function Module:OnEnteringWorld()
+    self:UpdateGilded()
+end
+
+function Module:CURRENCY_DISPLAY_UPDATE(_, currencyId)
+    -- Gilded Undermine Crest
+    if currencyId == 3110 then
+        C_Timer.After(2, function() self:UpdateGilded() end)
+    end
+end
+
+function Module:UpdateGilded()
+    local visInfo = CUIWM_GetSpellDisplayVisualizationInfo(6659)
+    if visInfo ~= nil and visInfo.spellInfo ~= nil and visInfo.spellInfo.tooltip ~= nil then
+        local gilded = strmatch(visInfo.spellInfo.tooltip, '(%d)/3') or '0'
+        Addon.charData.delvesGilded = tonumber(gilded)
+    end
 end
 
 function Module:UpdateScenario()
