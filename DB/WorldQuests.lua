40eb4203bf1ed2a47e8a10e133666f1c0f11c0d0
Freddie <freddie@wafflemonster.org>
2025-04-17 18:19:26 -0700

    Add more world quest zones

--- DB/WorldQuests.lua
+++ DB/WorldQuests.lua
@@ -7,12 +7,14 @@ Module.db.expansions = {
         id = 6,
         minimumLevel = 10,
         zones = {
+            627, -- Dalaran
             630, -- Azsuna
             634, -- Stormheim
             641, -- Val'sharah
             646, -- Broken Shore
             650, -- Highmountain
             680, -- Suramar
+            790, -- Eye of Azshara
             830, -- Krokuun
             882, -- Eredath
             885, -- Antoran Wastes
@@ -23,14 +25,20 @@ Module.db.expansions = {
         id = 7,
         minimumLevel = 10,
         zones = {
+            14, -- Arathi Highlands
+            62, -- Darkshore
             862, -- Zuldazar
             863, -- Nazmir
             864, -- Vol'dun
             895, -- Tiragarde Sound
             896, -- Drustvar
             942, -- Stormsong Valley
+            1161, -- Boralus
+            1165, -- Dazar'alor
             1355, -- Nazjatar
             1462, -- Mechagon Island
+            1527, -- Uldum
+            1530  -- Vale of Eternal Blossoms
         },
     },
     -- Shadowlands
@@ -41,7 +49,10 @@ Module.db.expansions = {
             1525, -- Revendreth
             1533, -- Bastion
             1536, -- Maldraxxus
+            1543, -- The Maw
             1565, -- Ardenweald
+            1961, -- Korthia
+            1970, -- Zereth Mortis
         },
     },
     -- Dragonflight
@@ -54,6 +65,8 @@ Module.db.expansions = {
             2023, -- Ohn'ahran Plains
             2024, -- Azure Span
             2025, -- Thaldraszus
+            2085, -- Primalist Tomorrow
+            2112, -- Valdrakken
             2133, -- Zaralek Cavern
             2151, -- The Forbidden Reach
             2200, -- Emerald Dream
@@ -68,9 +81,10 @@ Module.db.expansions = {
             2213, -- The City of Threads
             2214, -- The Ringing Deeps
             2215, -- Hallowfall
-            2116, -- The City of Threads - Lower
+            2216, -- The City of Threads - Lower
             2248, -- Isle of Dorn
             2255, -- Azj-Kahet
+            2256, -- Azj-Kahet - Lower
             2339, -- Dornogal
             2369, -- Siren Isle
             2346, -- Undermine
