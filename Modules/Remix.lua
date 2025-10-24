4e90250b175f280664e6015387e616c366968cd2
Freddie <freddie@wafflemonster.org>
2025-10-23 23:18:01 -0700

    Add remix limits unbound scanning

--- Modules/Remix.lua
+++ Modules/Remix.lua
@@ -2,22 +2,32 @@ local Addon = LibStub('AceAddon-3.0'):GetAddon('WoWthing_Collector')
 local Module = Addon:NewModule('Remix')
 
 
+local CT_GetConfigIDBySystemID = C_Traits.GetConfigIDBySystemID
+local CT_GetNodeInfo = C_Traits.GetNodeInfo
 local CUIWM_GetStatusBarWidgetVisualizationInfo = C_UIWidgetManager.GetStatusBarWidgetVisualizationInfo
 
+local ARTIFACT_SYSTEM_ID = 33
+local LIMITS_UNBOUND_NODE_ID = 108700
 local RESEARCH_WIDGET_ID = 7330
 
 function Module:OnEnable()
-    self:RegisterBucketEvent(
-        {
-            'QUEST_LOG_UPDATE', -- spammy quest log updates
-        },
-        2,
-        'UpdateResearch'
-    )
+    self:RegisterEvent('TRAIT_CONFIG_UPDATED', 'UpdateArtifact')
+    self:RegisterBucketEvent({ 'QUEST_LOG_UPDATE' }, 2, 'UpdateResearch')
 end
 
 function Module:OnEnteringWorld()
+    self:UpdateArtifact()
     self:UpdateResearch()
+end
+
+function Module:UpdateArtifact()
+    local configId = CT_GetConfigIDBySystemID(ARTIFACT_SYSTEM_ID)
+    if configId ~= nil then
+        local nodeInfo = CT_GetNodeInfo(configId, LIMITS_UNBOUND_NODE_ID)
+        if nodeInfo ~= nil then
+            Addon.charData.remixArtifactRank = nodeInfo.currentRank
+        end
+    end
 end
 
 function Module:UpdateResearch()
