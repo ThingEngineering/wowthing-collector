3615072edc4c91adb21ee085ac9def5a7bd6c0de
Freddie <freddie@thing.engineering>
2026-03-05 08:30:52 -0800

    Add midnight profession trait scanning

--- DB/ProfessionTraits.lua
+++ DB/ProfessionTraits.lua
@@ -6,55 +6,66 @@ Module.db.skillLines = {
     [171] = {
         2823, -- Dragon Isles
         2871, -- Khaz Algar
+        2906, -- Midnight
     },
     -- Blacksmithing
     [164] = {
         2822, -- Dragon Isles
         2872, -- Khaz Algar
+        2907, -- Midnight
     },
     -- Enchanting
     [333] = {
         2825, -- Dragon Isles
         2874, -- Khaz Algar
+        2909, -- Midnight
     },
     -- Engineering
     [202] = {
         2827, -- Dragon Isles
         2875, -- Khaz Algar
+        2910, -- Midnight
     },
     -- Herbalism
     [182] = {
         2832, -- Dragon Isles
         2877, -- Khaz Algar
+        2912, -- Midnight
     },
     -- Inscription
     [773] = {
         2828, -- Dragon Isles
         2878, -- Khaz Algar
+        2913, -- Midnight
     },
     -- Jewelcrafting
     [755] = {
         2829, -- Dragon Isles
         2879, -- Khaz Algar
+        2914, -- Midnight
     },
     -- Leatherworking
     [165] = {
         2830, -- Dragon Isles
         2880, -- Khaz Algar
+        2915, -- Midnight
     },
     -- Mining
     [186] = {
         2833, -- Dragon Isles
         2881, -- Khaz Algar
+        2916, -- Midnight
     },
     -- Skinning
     [393] = {
         2834, -- Dragon Isles
         2882, -- Khaz Algar
+        2917, -- Midnight
     },
     -- Tailoring
     [197] = {
         2831, -- Dragon Isles
         2883, -- Khaz Algar
+        2918, -- Midnight
     },
 }
