b8efdad10c8a75714e1d70c5b3e43f08eb6dd095
Freddie <freddie@thing.engineering>
2026-04-12 16:36:57 -0700

    Add mage portals/teleports to spell scanning

--- DB/Spells.lua
+++ DB/Spells.lua
@@ -27,7 +27,64 @@ Module.db.known = {
     61721, --  Rabbit
     61780, --  Turkey
     28271, --  Turtle
-    
+
+    -- Mage: Portal
+    281400, -- Boralus
+    224871, -- Dalaran - Broken Isles
+    53142, -- Dalaran - Northrend
+    11419, -- Darnassus
+    281402, -- Dazar'alor
+    446534, -- Dornogal
+    32266, -- Exodar
+    11416, -- Ironforge
+    11417, -- Orgrimmar
+    344597, -- Oribos
+    33691, -- Shattrath
+    35717, -- Shattrath
+    32267, -- Silvermoon (Burning Crusade)
+    1259194, -- Silvermoon City
+    49361, -- Stonard
+    176246, -- Stormshield
+    10059, -- Stormwind
+    49360, -- Theramore
+    11420, -- Thunder Bluff
+    88345, -- Tol Barad
+    88346, -- Tol Barad
+    11418, -- Undercity
+    395289, -- Valdrakken
+    132620, -- Vale of Eternal Blossoms
+    132626, -- Vale of Eternal Blossoms
+    176244, -- Warspear
+
+    -- Mage: Teleport
+    281403, -- Boralus
+    224869, -- Dalaran - Broken Isles
+    53140, -- Dalaran - Northrend
+    3565, -- Darnassus
+    281404, -- Dazar'alor
+    446540, -- Dornogal
+    32271, -- Exodar
+    193759, -- Hall of the Guardian
+    3562, -- Ironforge
+    3567, -- Orgrimmar
+    344587, -- Oribos
+    33690, -- Shattrath
+    35715, -- Shattrath
+    32272, -- Silvermoon
+    1259190, -- Silvermoon City
+    49358, -- Stonard
+    176248, -- Stormshield
+    3561, -- Stormwind
+    49359, -- Theramore
+    3566, -- Thunder Bluff
+    88342, -- Tol Barad
+    88344, -- Tol Barad
+    3563, -- Undercity
+    395277, -- Valdrakken
+    132621, -- Vale of Eternal Blossoms
+    132627, -- Vale of Eternal Blossoms
+    176242, -- Warspear
+
      -- Rogue
     210108, -- Detection
     
